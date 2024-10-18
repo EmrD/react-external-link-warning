b732a319339f15fd8d355c079025f34c3dee1a31
Emr <127445447+EmrD@users.noreply.github.com>
2024-10-18 20:06:10 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
-import Confirm from './confirm.jsx'
 import './index.css'
-import Internal from './internal.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} /> 
-        <Route path="/confirm-redirection" element={<Confirm />} />
-        <Route path="/internal-page" element={<Internal />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
